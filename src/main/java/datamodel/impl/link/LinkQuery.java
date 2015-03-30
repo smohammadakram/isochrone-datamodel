3d3ccccbb2c11076ca52172a17acde0310e8e992
Nikolaus Krismer <nikolaus.krismer@uibk.ac.at>
2015-03-30 21:46:45 +0200

    minor code/sql formatting

--- src/main/java/datamodel/impl/link/LinkQuery.java
+++ src/main/java/datamodel/impl/link/LinkQuery.java
@@ -33,7 +33,7 @@ final class LinkQuery {
 	private static final String INSERT_LINK_EDGE = "INSERT INTO time_expanded.%s_links(link_source, link_source_mode, link_destination, link_destination_mode) VALUES(?,?,?,?)";
 	private static final String INSERT_STREET_NODE = "INSERT INTO time_expanded.%s_street_nodes(node_id, node_geometry) VALUES (?, ST_GeomFromEWKT(?))";
 	private static final String INTER_BUS_LINKS = "SELECT bn1.node_id AS id_1, bn2.node_id AS id_2"
-		+ " FROM time_expanded.%s_bus_nodes bn1 JOIN  time_expanded.%s_bus_nodes bn2"
+		+ " FROM time_expanded.%s_bus_nodes bn1 JOIN time_expanded.%s_bus_nodes bn2"
 		+ " ON bn1.node_geometry = bn2.node_geometry AND bn1.node_id != bn2.node_id";
 	private static final String INTERSECTED_POINTS = "SELECT ST_LineInterpolatePoint(edge_geometry, %f) AS geom"
 		+ " FROM time_expanded.%s_street_edges"
@@ -62,7 +62,7 @@ final class LinkQuery {
 		+ " GROUP BY sn.node_id, se.edge_destination"
 		+ " ORDER BY sn.node_id";
 	private static final String STREET_NODES_STREET_OUTDEGREE = "SELECT sn.node_id, count(edge_source) AS out_d"
-		+ " FROM  time_expanded.%s_street_nodes sn JOIN time_expanded.%s_street_edges se"
+		+ " FROM time_expanded.%s_street_nodes sn JOIN time_expanded.%s_street_edges se"
 		+ " ON sn.node_id = se.edge_source"
 		+ " GROUP BY sn.node_id, se.edge_source"
 		+ " ORDER BY sn.node_id";
@@ -71,7 +71,7 @@ final class LinkQuery {
 	private static final String STREET_UPDATE_INSERT = "INSERT INTO time_expanded.%s_street_edges (edge_id, edge_source, edge_destination, edge_length, edge_geometry)"
 		+ " VALUES(%d, %d, %d, ST_Length(ST_Difference('%s', ST_Snap('%s', '%s', 1))), ST_Difference('%s', ST_Snap('%s', '%s', 1)))";
 	private static final String STREET_UPDATE_SELECT = "SELECT edge_id, edge_destination FROM time_expanded.%s_street_edges WHERE edge_geometry = '%s'";
-	private static final String STREET_UPDATE_UPDATE =  "UPDATE time_expanded.%s_street_edges"
+	private static final String STREET_UPDATE_UPDATE = "UPDATE time_expanded.%s_street_edges"
 		+ " SET edge_geometry = ST_Snap('%s', '%s', 1), edge_destination = %d, edge_length = ST_Length(ST_Snap('%s', '%s', 1))"
 		+ " WHERE edge_id=%d";
 	private static final String POINT_LOCATION = "SELECT edge_geometry, ST_Line_Locate_Point('%s', ST_GeomFromEWKT('%s')) AS p_loc"
