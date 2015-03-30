8c6a693a05c6635f65f0b39052b1b0b6753ee251
Nikolaus Krismer <nikolaus.krismer@uibk.ac.at>
2015-03-30 17:59:18 +0200

    introducing test helper class

--- src/test/java/datamodel/impl/ScriptGeneratorTest.java
+++ src/test/java/datamodel/impl/ScriptGeneratorTest.java
@@ -1,5 +1,7 @@
 package datamodel.impl;
 
+import datamodel.TestHelper;
+
 import java.io.File;
 import java.io.IOException;
 
@@ -8,23 +10,18 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class ScriptGeneratorTest {
-	private static final String CITY = "ttt";
-	private static final String PATH = "build/tmp/scripts";
 
 	@BeforeClass
 	public void setup() {
-		final File f = new File(PATH);
-		if (!f.exists() && !f.mkdirs()) {
-			Assert.fail("Could not generate output directory");
-		}
+		TestHelper.createTestOutputDirectory();
 	}
 
 	@Test
 	public void testByReplace() throws IOException {
 		final String testResource = "bus_network.sql";
 
-		final String output = PATH + File.separatorChar + CITY + "_" + testResource;
-		final ScriptGenerator sg = new ScriptGenerator(testResource, "<city>", CITY);
+		final String output = TestHelper.TEST_OUTPUT + File.separatorChar + TestHelper.TEST_CITY + "_" + testResource;
+		final ScriptGenerator sg = new ScriptGenerator(testResource, "<city>", TestHelper.TEST_CITY);
 		sg.writeScript(output);
 
 		Assert.assertTrue((new File(output)).exists());
